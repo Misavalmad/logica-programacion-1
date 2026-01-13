9ac9a2f28c1f8f48ec5c190afdd04922e70c507b
Misavalmad <misavalmad@gmail.com>
2026-01-13 16:10:44 -0600

    feat: added function with console log to check functionality

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ Debe solicitar al usuario 3 números por prompt y guardarlos en sus respectivas 
 Debe analizar los números, identificar cual es el número mayor, el número del centro y el número menor.
 Debe imprimir los números por consola o por el DOM ordenados de mayor a menor, y de menor a mayor.
 Debe ser capaz de identificar si los números son iguales e imprimir un mensaje por consola o por el DOM diciendo que los números son iguales. */
+
+let num1 = parseInt(prompt('Inserte un numero:'))
+let num2 = parseInt(prompt('Inserte un segundo numero:'))
+let num3 = parseInt(prompt('Inserte un tercer numero:'))
+
+function orderNumber(num1, num2, num3){
+    if (num1 === num2 & num2 === num3) {
+    } console.log(`Son iguales`);
+    
+}
+
+orderNumber()
